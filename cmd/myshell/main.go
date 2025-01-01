06c2a9a519618cdba8b0b8547d0a70705f86a2cd
jovaee <vaneedenj94@gmail.com>
2025-01-01 12:46:52 +0200

    handle invalid commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	val, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		fmt.Fprint(os.Stdout, "Failed to read command")
+	}
+
+	// Clean up val since it contains the delim char
+	command := strings.TrimSpace(val)
+
+	fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 }
